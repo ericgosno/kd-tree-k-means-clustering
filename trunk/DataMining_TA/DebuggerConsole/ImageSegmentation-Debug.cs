d8c3bc41a4630f02f611047918fc4a1b0860a251
eric.gosno@gmail.com <eric.gosno@gmail.com@e90c43d6-092b-d9c1-941b-10e1f140fceb>
2013-05-09 10:53:39 +0000

    - forget to add new cs files :)

---
+++ trunk/DataMining_TA/DebuggerConsole/ImageSegmentation-Debug.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Extension;
+using Clustering;
+using Clustering.Initialization;
+
+namespace DebuggerConsole
+{
+    public class ImageSegmentation_Debug
+    {
+        public static Dataset run()
+        {
+            List<Variables> inputVariables = new List<Variables>();
+            List<Variables> outputVariables = new List<Variables>();
+            List<Row> listRow = new List<Row>();
+
+            //Read Vocab
+            FileStream fileStream = null;
+            StreamReader streamReader = null;
+            string base_url = @"E:\5109100153 - Eric\tc\";
+
+            try
+            {
+                fileStream = new FileStream(base_url + @"segmentation.all.txt", FileMode.Open);
+                streamReader = new StreamReader(fileStream);
+
+                string line = streamReader.ReadLine();
+                string[] linex = line.Split(new char[1] { ',' });
+                bool yes = true;
+                foreach (string st in linex)
+                {
+                    if (yes)
+                    {
+                        CategoricalVariable outputVar = new CategoricalVariable(st);
+                        outputVar.ParamVariables["BRICKFACE"] = 1;
+                        outputVar.ParamVariables["SKY"] = 2;
+                        outputVar.ParamVariables["FOLIAGE"] = 3;
+                        outputVar.ParamVariables["CEMENT"] = 4;
+                        outputVar.ParamVariables["WINDOW"] = 5;
+                        outputVar.ParamVariables["PATH"] = 6;
+                        outputVar.ParamVariables["GRASS"] = 7;
+                        outputVariables.Add(outputVar as Variables);
+                        yes = false;
+                    }
+                    else
+                    {
+                        Variables news = new ContinueVariable(st);
+                        inputVariables.Add(news);                        
+                    }
+                }
+
+                while (true)
+                {
+                    line = streamReader.ReadLine();
+                    //if (line == null) continue;
+
+                    if (string.IsNullOrEmpty(line))
+                        break;
+
+                    char[] separator = new char[1] { ',' };
+                    linex = null;
+                    linex = line.Split(separator);
+                    Row newRow = new Row();
+                    yes = true;
+                    int numVar = 0;
+
+                    foreach(string st in linex)
+                    {
+                        if (!yes)
+                        {
+                            double ang = Convert.ToDouble(st);
+                            Cell newCell = new Cell(inputVariables[numVar], ang);
+                            inputVariables[numVar].RescaleLimitVariables(ang);
+                            newRow.InputValue.Add(inputVariables[numVar], newCell);
+                            numVar++;
+                        }
+                        else
+                        {
+                            newRow.RowIdentificator = st;
+                            newRow.OutputValue.Add(outputVariables[0], new Cell(outputVariables[0], (outputVariables[0] as CategoricalVariable).ParamVariables[st]));
+                            yes = false;
+                        }
+                    }
+                    listRow.Add(newRow);
+                }
+            }
+            finally
+            {
+                if (streamReader != null)
+                    streamReader.Close();
+                if (fileStream != null)
+                    fileStream.Close();
+            }
+            Console.WriteLine("Finish Read document!");
+            Random rnd = new Random();
+
+
+            Dataset dataset = new Dataset("Image Segmentation Dataset", listRow, inputVariables, outputVariables);
+            return dataset;
+
+            /*
+            IClustering clusterMethod = new ClusteringKMeans(10, 1000, false, ref rnd, dataset);
+            ClusteringResult clusters = clusterMethod.Run();
+            List<string> report = clusters.PrintCompleteResult();
+            for (int i = 0; i < report.Count; i++) Console.WriteLine(report[i]);
+            System.IO.File.WriteAllLines(base_url + @"output.txt", report);
+
+
+            IClustering clusterMethod2 = new ClusteringKMeans(10, 1000, false, ref rnd, dataset, new ForgyAlgorithm(10, dataset));
+            ClusteringResult clusters2 = clusterMethod2.Run();
+            List<string> report2 = clusters2.PrintCompleteResult();
+            for (int i = 0; i < report2.Count; i++) Console.WriteLine(report2[i]);
+            System.IO.File.WriteAllLines(base_url + @"output2.txt", report2);
+            string hold = Console.ReadLine();
+             */
+        }
+    }
+}
---
+++ trunk/DataMining_TA/DebuggerConsole/StandardClusteringTesting.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Extension;
+using Clustering;
+using Clustering.Initialization;
+
+namespace DebuggerConsole
+{
+    public class StandardClusteringTesting
+    {
+        public static void run(Dataset dataset, int numCluster)
+        {
+            Random rnd = new Random();
+            string datasetName = dataset.TitleDataset.Replace(' ','_').ToLower();
+            string base_url = @"E:\5109100153 - Eric\tc\";
+            List<string> totalReport = new List<string>();
+            double minDistortionKDTree = double.MaxValue;
+            double minDistortionForgy = double.MaxValue;
+            List<double> listSSEForgy = new List<double>();
+            List<double> maxNIGKDTree = new List<double>();
+            List<double> maxNIGForgy = new List<double>();
+            double meanDistortionForgy = 0.0;
+            double sdDistortionForgy = 0.0;
+
+            int ForgyLose = 0;
+            int ForgyWin = 0;
+            int ForgyDraw = 0;
+
+            for(int i = 0;i < dataset.OutputVariables.Count;i++)
+            {
+                maxNIGKDTree.Add(double.MinValue);
+                maxNIGForgy.Add(double.MinValue);
+            }
+
+            List<string> reportKDTree = new List<string>();
+            for (int j = 0; j < 2; j++)
+            {
+                IClustering clusterMethod = new ClusteringKMeans(numCluster, 1000, false, ref rnd, dataset, new KDTreeAlgorithm(numCluster, dataset, Convert.ToBoolean(j), true));
+                ClusteringResult clusters = clusterMethod.Run();
+                minDistortionKDTree = Math.Min(minDistortionKDTree, clusters.calculateSSE());
+                for (int i = 0; i < dataset.OutputVariables.Count; i++)
+                {
+                    maxNIGForgy[i] = Math.Max(maxNIGForgy[i], clusters.CalculateNIG(dataset.OutputVariables[i]));
+                }
+
+                List<string> report = clusters.PrintCompleteResult();
+                for (int i = 0; i < report.Count; i++) Console.WriteLine(report[i]);
+                reportKDTree.AddRange(report);
+            }
+            System.IO.File.WriteAllLines(base_url + datasetName + @".KDTree.output.txt", reportKDTree);
+
+            List<string> reportForgy = new List<string>();
+            for (int j = 0; j < 15; j++)
+            {
+                IClustering clusterMethod2 = new ClusteringKMeans(numCluster, 1000, false, ref rnd, dataset, new ForgyAlgorithm(numCluster, dataset));
+                ClusteringResult clusters2 = clusterMethod2.Run();
+                double SSENow = clusters2.calculateSSE();
+                listSSEForgy.Add(SSENow);
+                minDistortionForgy = Math.Min(minDistortionForgy, SSENow);
+                meanDistortionForgy += SSENow;
+                for (int i = 0; i < dataset.OutputVariables.Count; i++)
+                {
+                    maxNIGKDTree[i] = Math.Max(maxNIGKDTree[i], clusters2.CalculateNIG(dataset.OutputVariables[i]));
+                }
+
+                if (Math.Abs(SSENow - minDistortionKDTree) < 1e-3) ForgyDraw++;
+                else if (SSENow < minDistortionKDTree) ForgyWin++;
+                else if (SSENow > minDistortionKDTree) ForgyLose++;
+
+                List<string> report2 = clusters2.PrintCompleteResult();
+                for (int i = 0; i < report2.Count; i++) Console.WriteLine(report2[i]);
+                reportForgy.AddRange(report2);
+            }
+            System.IO.File.WriteAllLines(base_url + datasetName + @".Forgy.output.txt", reportForgy);
+
+            meanDistortionForgy = meanDistortionForgy / 15;
+            for (int i = 0; i < 15; i++)
+            {
+                double dist = listSSEForgy[i] - meanDistortionForgy;
+                sdDistortionForgy += (dist * dist);
+            }
+            sdDistortionForgy = sdDistortionForgy / 15.0;
+            sdDistortionForgy = Math.Sqrt(sdDistortionForgy);
+
+            totalReport.Add("DATASET : ");
+            totalReport.AddRange(dataset.PrintDatasetDetail());
+            totalReport.Add("Min SSE KD-Tree : " + minDistortionKDTree);
+            totalReport.Add("Min SSE Forgy : " + minDistortionForgy);
+            totalReport.Add("Mean SSE Forgy : " + meanDistortionForgy);
+            totalReport.Add("Standard Deviation SSE Forgy : " + sdDistortionForgy);
+            totalReport.Add("Forgy vs KDTree (w-d-l) : " + ForgyWin + "-" + ForgyDraw + "-" + ForgyLose);
+            for (int i = 0; i < dataset.OutputVariables.Count; i++)
+            {
+                totalReport.Add("Max " + dataset.OutputVariables[i].NameVariables + " NIG KD-Tree: " + maxNIGKDTree[i]);
+                totalReport.Add("Max " + dataset.OutputVariables[i].NameVariables + " NIG Forgy: " + maxNIGForgy[i]);
+            }
+            System.IO.File.WriteAllLines(base_url + datasetName + @".total.output.txt", totalReport);
+        }
+    }
+}
